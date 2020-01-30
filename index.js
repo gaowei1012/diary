4112274ef57c321788e1d2d87f51929b8164dc19
gaowei1012 <mingwei2222@126.com>
2020-01-30 17:08:35 +0800

    init project

---
+++ index.js
@@ -0,0 +1,17 @@
+/*
+ * @Author: your name
+ * @Date: 2020-01-30 12:29:12
+ * @LastEditTime : 2020-01-30 15:51:59
+ * @LastEditors  : Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /diary/index.js
+ */
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import App from './js/App';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => App);
